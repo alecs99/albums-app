61d83fba7bc7ac090a9387e8e54c61f183c80642
alecsandru.ciobanu <ciobanu.alecsandru@gmail.com>
2023-04-22 14:07:23 +0300

    Initial commit + adding home screen

---
+++ src/test/java/com/facultate/albumstore/AlbumstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facultate.albumstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
